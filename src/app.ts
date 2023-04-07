9ce752ec815495888fd0b3d01f24c4f1849a51dc
gpmarchi <gustavomarchi@gmail.com>
2023-04-07 11:54:13 -0300

    refactor: extract error handler function to new file

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,9 @@
 import fastify from 'fastify'
-import { ZodError } from 'zod'
-import { env } from './env'
 import { appRoutes } from './http/routes'
-import { AppError } from './shared/errors/app-error'
+import { errorHandler } from './shared/errors/error-handler'
 
 export const app = fastify()
 
 app.register(appRoutes)
 
-app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: 'Validation error.', issues: error.format() })
-  }
-
-  if (error instanceof AppError) {
-    return reply.status(error.statusCode).send({ message: error.message })
-  }
-
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
-  } else {
-    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
-  }
-
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
+app.setErrorHandler(errorHandler)
