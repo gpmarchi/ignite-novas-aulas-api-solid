bd11b4380757aa8b9f429d2985aba7dd956ab737
gpmarchi <gustavomarchi@gmail.com>
2023-04-17 13:14:19 -0300

    feat: authentication use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,38 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+interface AuthenticateUserCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUserCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUserCaseRequest): Promise<AuthenticateUserCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.passwordHash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
---
+++ src/use-cases/errors/invalid-credentials-error.ts
@@ -0,0 +1,7 @@
+import { AppError } from '@/shared/errors/app-error'
+
+export class InvalidCredentialsError extends AppError {
+  constructor() {
+    super('Invalid credentials.', 401)
+  }
+}
