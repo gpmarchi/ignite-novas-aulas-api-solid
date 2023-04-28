ae8847a30442041ef2d8060bd233a4e4d2047ccc
gpmarchi <gustavomarchi@gmail.com>
2023-04-28 14:18:15 -0300

    feat: create gyms

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', async () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Javascript Gym',
+      description: null,
+      phone: null,
+      latitude: -23.6106794,
+      longitude: -46.6222736,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
