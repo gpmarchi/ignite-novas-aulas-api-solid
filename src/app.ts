458e0eae8ac7ac46544f67cb40bf86ab29de4ddb
gpmarchi <gustavomarchi@gmail.com>
2023-05-10 16:05:10 -0300

    feat: add authentication via jwt

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { env } from './env'
 import { appRoutes } from './http/routes'
 import { errorHandler } from './shared/errors/error-handler'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
+  app.get('/me', profile)
 
   app.post('/sessions', authenticate)
 }
