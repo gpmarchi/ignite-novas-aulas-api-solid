7a23f4d3cf51b50bbccbdfedd2e5cd59028f1af9
gpmarchi <gustavomarchi@gmail.com>
2023-05-23 15:34:57 -0300

    feat: add new routes file related to gyms

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { env } from './env'
+import { gymsRoutes } from './http/controllers/gyms/route'
 import { usersRoutes } from './http/controllers/users/routes'
 import { errorHandler } from './shared/errors/error-handler'
 
@@ -11,5 +12,6 @@ app.register(fastifyJwt, {
 })
 
 app.register(usersRoutes)
+app.register(gymsRoutes)
 
 app.setErrorHandler(errorHandler)
