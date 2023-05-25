9ea12a746132dbb2e774d361ca5d340f0d72df8b
gpmarchi <gustavomarchi@gmail.com>
2023-05-25 14:04:58 -0300

    test: add search gyms e2e test suite

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,56 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Some description',
+        phone: '123112321',
+        latitude: -23.6106794,
+        longitude: -46.6222736,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Typescript Gym',
+        description: 'Some description',
+        phone: '123112321',
+        latitude: -23.6106794,
+        longitude: -46.6222736,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        query: 'Javascript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Javascript Gym',
+      }),
+    ])
+  })
+})
