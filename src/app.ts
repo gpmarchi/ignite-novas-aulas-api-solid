1cad77d5cdd16f6b92c1a019898cf4180a6bdea3
gpmarchi <gustavomarchi@gmail.com>
2023-05-25 14:01:47 -0300

    fix: register missing check-ins router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { env } from './env'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 import { gymsRoutes } from './http/controllers/gyms/route'
 import { usersRoutes } from './http/controllers/users/routes'
 import { errorHandler } from './shared/errors/error-handler'
@@ -13,5 +14,6 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler(errorHandler)
